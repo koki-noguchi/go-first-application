f166587b337b4c3aba1b9cc34135bee386eb2f48
koki_noguchi <koki.noguchi0202@gmail.com>
2021-02-28 15:42:24 +0900

    graphqlの準備

--- backend/app/go.mod
+++ backend/app/go.mod
@@ -2,4 +2,8 @@ module app
 
 go 1.13
 
-require github.com/labstack/echo/v4 v4.2.0
+require (
+	github.com/99designs/gqlgen v0.13.0
+	github.com/labstack/echo/v4 v4.2.0
+	github.com/vektah/gqlparser/v2 v2.1.0
+)
--- backend/app/main.go
+++ backend/app/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"app/resolver"
 	"net/http"
 	"os"
 
+	"github.com/99designs/gqlgen/handler"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -13,6 +15,7 @@ func main() {
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+	e.Use(middleware.Gzip())
 
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{os.Getenv("CORS_ALLOW_ORIGIN")},
@@ -21,6 +24,16 @@ func main() {
 
 	e.GET("/", func(c echo.Context) error {
 		return c.NoContent(http.StatusOK)
+	})
+
+	e.POST("/graphql", func(c echo.Context) error {
+		config := resolver.Config{
+			Resolvers: resolver.New(),
+		}
+		h := handler.GraphQL(resolver.NewExecutableSchema(config))
+		h.ServeHTTP(c.Response(), c.Request())
+
+		return nil
 	})
 
 	e.HideBanner = true
