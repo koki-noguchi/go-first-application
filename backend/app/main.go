9f77d14681bd38e36142ee8046461c845e3d936b
koki_noguchi <koki.noguchi0202@gmail.com>
2021-02-28 10:57:11 +0900

    環境構築

---
+++ backend/app/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{os.Getenv("CORS_ALLOW_ORIGIN")},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	}))
+
+	e.GET("/", func(c echo.Context) error {
+		return c.NoContent(http.StatusOK)
+	})
+
+	e.HideBanner = true
+	e.HidePort = true
+	e.Logger.Fatal(e.Start(":3000"))
+}
