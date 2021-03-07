bd1e20dd2de25c622c79eb6ebb25c1063c1e4107
koki_noguchi <koki.noguchi0202@gmail.com>
2021-03-07 13:12:28 +0900

    graphqlの設定を初期化

--- backend/app/model/models_gen.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type CreateWorryInput struct {
-	UserID     int     `json:"user_id"`
-	Title      string  `json:"title"`
-	Notes      *string `json:"notes"`
-	CategoryID int     `json:"category_id"`
-}
