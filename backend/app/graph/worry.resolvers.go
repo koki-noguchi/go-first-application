3f2d05c238bf2d922eae0f4f97159f73b796a540
koki noguchi <koki.noguchi0202@gmail.com>
2021-03-23 21:49:20 +0900

    worryの詳細を実装

--- backend/app/graph/worry.resolvers.go
+++ backend/app/graph/worry.resolvers.go
@@ -4,14 +4,24 @@ package graph
 // will be copied through when generating and any unknown code will be moved to the end.
 
 import (
+	"app/config"
 	"app/graph/generated"
 	"app/models"
 	"context"
-	"fmt"
 )
 
 func (r *worryResolver) User(ctx context.Context, obj *models.Worry) (*models.User, error) {
-	panic(fmt.Errorf("not implemented"))
+	db := config.DB()
+
+	var user models.User
+	if err := db.Where("id = ?", obj.UserID).First(&user).Error; err != nil {
+		return nil, err
+	}
+
+	return &models.User{
+		ID:   user.ID,
+		Name: user.Name,
+	}, nil
 }
 
 // Worry returns generated.WorryResolver implementation.
