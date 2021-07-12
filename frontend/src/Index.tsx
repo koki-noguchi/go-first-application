cca936a8c854534ce00f6e7bfa4daf9f9557c6cd
koki noguchi <koki.noguchi0202@gmail.com>
2021-07-12 21:28:00 +0900

    apollo cilentの初期化

--- frontend/src/Index.tsx
+++ frontend/src/Index.tsx
@@ -2,14 +2,28 @@ import React from "react";
 import ReactDom from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import 'semantic-ui-css/semantic.min.css'
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    useQuery,
+    gql
+  } from "@apollo/client";
 
 import { Router } from "./router/Router";
 
+const client = new ApolloClient({
+    uri: 'http://localhost:3000/graphql',
+    cache: new InMemoryCache()
+  });
+
 export default function App() {
     return(
-        <BrowserRouter>
-            <Router></Router>
-        </BrowserRouter>
+        <ApolloProvider client={client}>
+            <BrowserRouter>
+                <Router></Router>
+            </BrowserRouter>
+        </ApolloProvider>
     )
 }
 
