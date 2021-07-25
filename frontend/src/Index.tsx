b5c3787e867b4cf962bcc1290f1cfc62d32f67c4
koki noguchi <koki.noguchi0202@gmail.com>
2021-07-25 10:33:40 +0900

    feat worryを登録できるようにした

--- frontend/src/Index.tsx
+++ frontend/src/Index.tsx
@@ -5,23 +5,41 @@ import 'semantic-ui-css/semantic.min.css'
 import {
     ApolloClient,
     InMemoryCache,
-    ApolloProvider,
-    useQuery,
-    gql
+    HttpLink,
+    ApolloProvider
   } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
 
 import { Router } from "./router/Router";
+import { AuthRouter } from "./router/AuthRouter";
+import firebase from 'firebase';
 
-const client = new ApolloClient({
-    uri: 'http://localhost:3000/graphql',
+const httpLink = new HttpLink({ uri: 'http://localhost:3000/graphql' });
+
+const authLink = setContext((_, { headers }) => {
+    return firebase.auth().currentUser?.getIdToken().then((token) => {
+        return {
+            headers: {
+                ...headers,
+                Authorization: token ? `Bearer ${token}` : "",
+            }
+        }
+    })
+});
+
+const client =  new ApolloClient({
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache()
   });
+
 
 export default function App() {
     return(
         <ApolloProvider client={client}>
             <BrowserRouter>
                 <Router></Router>
+                    <AuthRouter></AuthRouter>
             </BrowserRouter>
         </ApolloProvider>
     )
--- frontend/src/components/pages/CreateWorry.tsx
+++ frontend/src/components/pages/CreateWorry.tsx
@@ -1,19 +1,67 @@
-import React from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
+import styled from 'styled-components'
 import { Link } from 'react-router-dom';
-import { Button, Checkbox, Form } from 'semantic-ui-react';
+import { Button, Form } from 'semantic-ui-react';
+import { useCreateWorryMutation } from '../../generated/graphql';
+import firebase from 'firebase';
+
 
 export const CreateWorry = () => {
+    const [title, setTitle] = useState('');
+    const [note, setNote] = useState('');
+
+    const onChangeTitle = (e: ChangeEvent<HTMLInputElement>) => setTitle(e.target.value);
+    const onChangeNote = (e: ChangeEvent<HTMLTextAreaElement>) => setNote(e.target.value);
+
+    const [createWorry] = useCreateWorryMutation({
+        variables: {
+            title: title,
+            notes: note
+        }
+    });
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        createWorry();
+    }
+
     return (
         <>
         <Link to='/'>home</Link>
-        <Form>
-            <Form.Field>
-                <label>タイトル</label>
-                <input placeholder='First Name' />
-            </Form.Field>
-            <Form.TextArea label='不安なことを書いてみよう' placeholder='Tell us more about you...' />
-            <Button type='submit'>Submit</Button>
-        </Form>
+        <SDiv>
+            <SForm onSubmit={handleSubmit}>
+                <Form.Field>
+                    <label>タイトル</label>
+                    <input value={title} onChange={onChangeTitle} placeholder='心配事・不安・嫌だったことを一言で表すと？' />
+                </Form.Field>
+                <Form.Field>
+                    <label>心配事・不安・嫌だったこと</label>
+                    <textarea
+                        value={note}
+                        onChange={onChangeNote}
+                        style={{height: '50vh', resize: 'none'}}
+                        placeholder='今日１日にどんな心配事・不安・嫌だったことがありましたか？例えば、発表会で失敗しないか不安だった・自分のミスで先輩に迷惑をかけて凹んだといったように。'
+                    ></textarea>
+                </Form.Field>
+                <div>
+                    <Button type='submit'>Submit</Button>
+                </div>
+            </SForm>
+        </SDiv>
         </>
     )
 }
+
+const SForm = styled(Form)`
+    width: 70vh;
+    background-color: #CCFFCC;
+    padding: 15px;
+    border-radius: 10px;
+`
+
+const SDiv = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 90vh;
+`
